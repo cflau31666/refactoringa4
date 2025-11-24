bd57fbc89a1e48870040d87ae787f1fd9b2811c1
Cheuk Fung <cheukfunglau2024@cdnis.edu.hk>
2025-11-24 17:36:26 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,39 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
+    /**
+     * Constructs a Performance with a play ID and audience size.
+     *
+     * @param playID   the ID of the play
+     * @param audience the audience size
+     */
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Returns the play ID for this performance.
+     *
+     * @return the play ID
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Returns the audience size for this performance.
+     *
+     * @return the audience size
+     */
+    public int getAudience() {
+        return audience;
     }
 }
